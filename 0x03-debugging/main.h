7bef8da4b303c82a18b568afd149aa52f54ecc5a
feyiFX <oguntoyinbofeyisayo01@gmail.com>
2022-09-15 09:47:42 -0700

    main.h file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void _putchar(char);
+int positive_or_negative(char);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
